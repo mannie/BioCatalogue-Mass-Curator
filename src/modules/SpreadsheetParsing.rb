521cda41e955f8dcdb294e62e6aae585b585441f
Mannie Tagarira <mannie@mygrid.org.uk>
2010-06-18 17:43:07 +0100

    - updated Spreadsheet parsing module to take into account multiple annotations in different worksheets

--- src/modules/SpreadsheetParsing.rb
+++ src/modules/SpreadsheetParsing.rb
@@ -28,82 +28,92 @@ module SpreadsheetParsing
       
       Spreadsheet.client_encoding = 'UTF-8'
       @workbook = Spreadsheet.open(filePath)
-
-      bulk_annotations = []
       
-      # iterate each row of the worksheet
-      @workbook.worksheets[0].each do |row|
-        if row == @workbook.worksheets[0].first
-          @header = row.to_a
-        else # all other rows
-          # set up temp storage vats
-          entry = {}
-          annotations = {}
-          
-          id, resource, name = nil, nil, nil
-          description, tags, examples = nil, nil, nil
-          
-          # fill in vars with the spreadsheet values
-          @header.size.times do |cell|
-            case @header[cell]
-              when "ID"
-                break unless row[cell]
-                id = row[cell]
-              when "Type"
-                resource = Application.resourceNameFor(row[cell])
-
-                if resource=='soap_services'
-                  uri = Application.weblinkWithIDForResource(id)
-                  @service = Application.serviceWithURI(uri.to_s)
-                  @service.fetchComponents
-
-                  entry.merge!('resource' => @service.variantURI.to_s)
-                else
-                  @component = @service.components[id] if resource==
-                      'soap_operations'
-
-                  uri = Application.weblinkWithIDForResource(id, resource)
+      # init work vars
+      bulkAnnotations = []      
+      
+      @workbook.worksheets.each do |worksheet| # iterate each worksheet
+        worksheet.each do |row| # iterate each row of the worksheet
+          if row==worksheet.first
+            @header = row.to_a
+            raise "Incompatible spreadsheet format" unless @header==SpreadsheetConstants.HEADER
+          else # all other rows
+            # set up temp storage vars
+            entry = {}
+            annotations = {}
+            
+            description, tags, examples = nil, nil, nil
+            
+            # fill in vars with the spreadsheet values
+            @header.size.times do |cell|
+              case cell
+                when SpreadsheetConstants.column(:id)
+                  @id = row[cell] if row[cell]
+                when SpreadsheetConstants.column(:type)
+                  unless @id
+                    @resource, @service, @component = nil, nil. nil
+                    break
+                  end
                   
-                  entry.merge!('resource' => uri.to_s)
-                end # if else resource=='soap_services'
-              when "Description"
-                break unless row[cell]
-                
-                originalDescrip = case resource
-                                    when 'soap_services'
-                                      @service.description
-                                    when 'soap_operations'
-                                      @component.description
-                                    when 'soap_inputs'
-                                      @component.inputs[id].description
-                                    when 'soap_outputs'
-                                      @component.outputs[id].description
-                                  end # case resource
-                
-                altered = originalDescrip.chomp.strip != row[cell].chomp.strip
-                annotations.merge!('description' => row[cell]) if altered
-              when "Tags"
-                break unless row[cell]
-                
-                annotations.merge!('tag' => row[cell].to_s.split(','))
-              when "Examples"
-                break unless row[cell]
-                
-                annotations.merge!('example_data' => row[cell].to_s.split(','))
-            end # case property
-          end # @header.each
-          
-          # consolidate the entry
-          unless annotations.empty?
-            entry.merge!('annotations' => annotations)
-            bulk_annotations << entry
-          end
-          
-        end # if else row == first        
-      end # each row in the workboow
+                  @resource = Application.resourceNameFor(row[cell]) if
+                      row[cell]
+
+                  if @resource=='soap_services'
+                    uri = Application.weblinkWithIDForResource(@id)
+                    @service = Application.serviceWithURI(uri.to_s)
+                    @service.fetchComponents
+
+                    entry.merge!('resource' => @service.variantURI.to_s)
+                  else
+                    @component = @service.components[@id] if @resource==
+                        'soap_operations'
+
+                    uri = Application.weblinkWithIDForResource(@id, @resource)
+                    
+                    entry.merge!('resource' => uri.to_s)
+                  end # if else resource=='soap_services'
+                when SpreadsheetConstants.column(:newDescriptions)
+                  break unless @id
+                  
+                  originals = case @resource
+                                when 'soap_services'
+                                  @service.descriptions
+                                when 'soap_operations'
+                                  @component.descriptions
+                                when 'soap_inputs'
+                                  @component.inputs[@id].descriptions
+                                when 'soap_outputs'
+                                  @component.outputs[@id].descriptions
+                              end # case resource
+                  
+                  isNew = row[cell] && !originals.include?(row[cell])
+                  annotations.merge!('description' => row[cell]) if isNew
+                when SpreadsheetConstants.column(:tags)
+                  break unless @id
+                  
+                  annotations.merge!('tag' => row[cell].to_s.split(',')) if 
+                      row[cell]
+                when SpreadsheetConstants.column(:examples)
+                  break unless @id
+                  
+                  allow = row[cell] && !@resource=~/(service|operation)/i
+                  annotations.merge!(
+                      'example_data' => row[cell].to_s.split(',')) if allow
+              end # case property
+            end # @header.each
+            
+            # consolidate the entry
+            unless annotations.empty?
+              entry.merge!('annotations' => annotations)
+              bulkAnnotations << entry
+            end
+            
+          end # if else row == first        
+        end # worksheet.each
+      end # workbook.worksheets.each
       
-      content = {'bulk_annotations' => bulk_annotations}
-      return (bulk_annotations.empty? ? '' : content.to_json)
+      content = {'bulk_annotations' => bulkAnnotations}
+      return (bulkAnnotations.empty? ? '' : content.to_json)
 
     rescue Exception => ex
       log('e', ex)
