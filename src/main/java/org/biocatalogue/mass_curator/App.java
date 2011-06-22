b47f6ad42e861b7d487be721438da86ff96b7663
Mannie Tagarira <mannie@dimensionsunmarked.com>
2011-06-22 13:00:19 +0100

    init

---
+++ src/main/java/org/biocatalogue/mass_curator/App.java
@@ -0,0 +1,13 @@
+package org.biocatalogue.mass_curator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
