b796f23d4aeb58f3a173d73aa579f4afd7001a97
Mannie Tagarira <mannie@dimensionsunmarked.com>
2011-06-22 13:54:01 +0100

    added a generic interface to represent resource indexes - renamed App.java to AppLauncher.java

---
+++ biocatalogue-api/src/main/java/models/interfaces/ResourceIndex.java
@@ -0,0 +1,31 @@
+package models.interfaces;
+
+import models.interfaces.abstracts.IdentifiableEntity;
+
+public interface ResourceIndex {
+
+  public String getSearchQuery();
+
+  // ---
+
+  public int getNumberOfItemsPerPage();
+
+  public int getTotalNumberOfItems();
+
+  // ---
+
+  public int getCurrentPage();
+
+  public int getNumberOfPages();
+
+  // --
+
+  public String getSortedBy();
+
+  public String getSortOrder();
+
+  // --
+  
+  public IdentifiableEntity[] getResults();
+
+}
---
+++ src/main/java/org/biocatalogue/mass_curator/AppLauncher.java
@@ -0,0 +1,13 @@
+package org.biocatalogue.mass_curator;
+
+/**
+ * Hello world!
+ * 
+ */
+public class AppLauncher {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+
+}
